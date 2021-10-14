28b92c3b967f403b44d16102a9448972bdd33483
sonali <sonalic4qa@gmail.com>
2021-10-14 15:07:26 -0400

    Added Cart Page

---
+++ src/main/java/com/qa/opencart/pages/cart.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class cart {
+
+	
+	public static void cartPage() {
+		System.out.println("Cart Page");
+		
+	}
+	
+	public void addToCart() {
+		System.out.println("Add to Cart Page");
+	}
+}
